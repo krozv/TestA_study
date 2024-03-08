eb0068098f3855b4b75e405ee5a9a44a63e23cb3
HoseaKim1025 <hoseakim1025@gmail.com>
2024-03-09 02:15:00 +0900

    HoseaKim 0309

--- 240302/디저트카페_HoseaKim_S_2105.py
+++ 240302/디저트카페_HoseaKim_S_2105.py
@@ -1,53 +1,21 @@
 # 디저트 카페 swea 2105
-def f():
-    global max_cnt
-    for i in range(1, n-1):
-        for j in range(1, n-1):
-
-            # visited 길이 = 모든 디저트 종류 = 1 ~ 100
-            v = [0] * 101
-            for d in range(2, 4):      # ← ↑
-                if v[arr[i + dx[d]][j + dy[d]]] == 0:
-                    v[arr[i + dx[d]][j + dy[d]]] = 1
-            if sum(v) == 2:
-                k = 0
-                while i+k < n-1 and j+k < n-1:      # ↘
-                    Ri, Rj = i+k, j+k
-                    for d in range(2):                  # → ↓
-                        if v[arr[Ri + dx[d]][Rj + dy[d]]] == 0:
-                            v[arr[Ri + dx[d]][Rj + dy[d]]] = 1
-                        else:
-                            break
-                    else:
-                        k += 1
-                        continue
-                    break
-                if max_cnt < k:
-                    max_cnt = k
-                    if max_cnt == n - 1 - i:
-                        return
-
-            v = [0] * 101
-            for d in (0, 3):      # ↑ →
-                if v[arr[i + dx[d]][j + dy[d]]] == 0:
-                    v[arr[i + dx[d]][j + dy[d]]] = 1
-            if sum(v) == 2:
-                k = 0
-                while i+k < n-1 and j-k >= 1:           # ↙
-                    Li, Lj = i+k, j-k
-                    for d in range(1, 3):                  # ↓ ←
-                        if v[arr[Li + dx[d]][Lj + dy[d]]] == 0:
-                            v[arr[Li + dx[d]][Lj + dy[d]]] = 1
-                        else:
-                            break
-                    else:
-                        k += 1
-                        continue
-                    break
-                if max_cnt < k:
-                    max_cnt = k
-                    if max_cnt == n - 1 - i:
-                        return
+def dfs(x, y):
+    for d in range(2):
+        next = [[x, y], [x, y]]
+        lenth = [0, 0]
+        while 0 <= next[d][0] < n and 0 <= next[d][1] < n:
+            next[d][0] += dx[d]
+            next[d][1] += dy[d]
+            if v[arr[next[d][0]][next[d][1]]] == 0:
+                v[arr[next[d][0]][next[d][1]]] = 1
+                lenth[d] += 1
+            else:
+                break
+        
+        if lenth[d] == 0:
+            return 0
+            
+            
 
 
 T = int(input())
@@ -57,19 +25,13 @@ for case in range(1, T+1):
     # 디저트 종류는 1 ~ 100
     arr = [list(map(int, input().split())) for _ in range(n)]
 
-    dx = (0, 1, 0, -1)
-    dy = (1, 0, -1, 0)
-
-    # 최대 stack = 최대 디저트 종류 = n-1 * 2
-    stack = [0] * (n - 1) * 2
-    top = -1
-
-    max_cnt = 0
-    ans = -1
-    f()
-
-    if max_cnt:
-        ans = (max_cnt * 2) + 2
+    dx = (1, -1)
+    dy = (1, 1)
+    for i in range(n-2):
+        for j in range(1, n-1):
+            v = [0] * 101
+            v[arr[i][j]] = 1
+            temp = dfs(i, j) * 2
 
     print(f'#{case}', ans)
 
